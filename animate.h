b07860a8358ca2823c59b1c3a313fb7d6784a0a0
humichen <43415952+humichen@users.noreply.github.com>
2018-12-29 21:24:23 +0800

    Add files via upload

--- animate.h
+++ animate.h
@@ -1,4 +1,4 @@
-//face矩陣名：0.angry1  1.angry2  2.blink2  3.blink3  4.happy1  5.happy2  6.normal1  7.normal2  8.normal3  9.unhappy1   10.unhappy2  11.unhappy3  12.hungry1  13.hungry2  14.wakeup1  15.wakeup2  16.wakeup3  17.happy_11  18.happy_12
+//face矩陣名：0.angry1  1.angry2  2.blink2  3.blink3  4.happy1  5.happy2  6.normal1  7.normal2  8.normal3  9.unhappy1   10.unhappy2  11.unhappy3  12.hungry1  13.hungry2  14.wakeup1  15.wakeup2  16.wakeup3  17.happy_11  18.happy_12  19.shark1  20.shark2
 //函式名稱：Wakeup()  Angry()  Blink()  Happy()  Happy1()  Unhappy()  Normal()  Hungry()  Left()  Right()
 #include"picture.h"
 void Wakeup(){
@@ -142,3 +142,15 @@ void Hungry(){
     }
  } //肚子餓不開心
 
+void Hungry(){
+    for (int i=0; i<=3; i++) {
+        display.clear();
+        display.drawXbm(0,0, 128, 64, face[19]);
+        display.display();
+        delay(300);
+        display.clear();
+        display.drawXbm(0,0, 128, 64, face[20]);
+        display.display();
+        delay(300);
+    }
+    } //肚子餓不開心
